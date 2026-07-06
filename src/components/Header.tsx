9480f423dfba09d7be38a01fe269c732dd3cc3fd
S <2359572@cognizant.com>
2026-07-06 16:39:24 +0530

    error fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { NAV_ITEMS, COMPANY } from '../data'
+import { NAV_ITEMS } from '../data'
 import { useTheme } from '../context/ThemeContext'
 
 // Icons
